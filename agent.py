3cf4d1ce457590486d5c69f3a20c21fe5aa1a14a
Wen Dong <wen.dong@microfocus.com>
2023-12-05 11:13:58 +0800

    switche to agent

--- agent.py
+++ agent.py
@@ -3,7 +3,6 @@ import getpass
 import os, sys
 import pandas as pd 
 
-from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.prompts import PromptTemplate, ChatPromptTemplate, MessagesPlaceholder
 
@@ -19,6 +18,8 @@ from langchain.schema import StrOutputParser
 from langchain.schema.runnable import RunnablePassthrough
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.schema.messages import AIMessage, HumanMessage
+
+import dotenv
 
 
 def get_llm():
@@ -49,8 +50,9 @@ def condense_question(input: dict):
     else:
         return input["question"]
 
-def main(argv):
-    os.environ["OPENAI_API_KEY"] = argv[0]
+def main():
+    
+    dotenv.load_dotenv()
 
     # os.environ["LANGCHAIN_TRACING_V2"] = "true"
     # still beta not generally available.
@@ -60,32 +62,68 @@ def main(argv):
     df['page_content'] = "Title: " + df["title"] + "\n" + \
                         "Content: " + df["content"] + "\n" + \
                         "Source: " + df["source"]
+    df = df[['page_content', 'source']]
 
     docs = DataFrameLoader(df, page_content_column='page_content').load()
     db = Chroma.from_documents(docs, OpenAIEmbeddings())
-    retriever = db.as_retriever(search_type="similarity_score_threshold", search_kwargs={"k": 2, "score_threshold":.18})
+    retriever = db.as_retriever(search_type="similarity_score_threshold", search_kwargs={"k": 1, "score_threshold":.7})
 
-    prompt = PromptTemplate.from_template("""Given the following extracted parts of a RnD internal knowledge database and a question, create a final answer with the knowledge document link as source ("SOURCE").
-        If you don't know the answer, just say that you don't know. Don't try to make up an answer. ALWAYS return a "SOURCE" part in your answer.
-        Use numbered list whenever possible
-        Question: {question} 
-        Context: {context}
-        Answer:""")
+    from langchain.agents.agent_toolkits import create_retriever_tool
+    tool = create_retriever_tool(
+        retriever,
+        "internal_knowledge_resources",
+        "Searches and returns documents from the internal knowledge resources.",
+    )
+    tools = [tool]
 
-    rag_chain = (
-        RunnablePassthrough.assign(context=condense_question | retriever | format_docs)
-        | prompt
-        | get_llm()
-        | StrOutputParser())
+    from langchain.agents.agent_toolkits import create_conversational_retrieval_agent
 
-    chat_history = []
+    # This is needed for both the memory and the prompt
+    memory_key = "history"
+    from langchain.agents.openai_functions_agent.agent_token_buffer_memory import (
+        AgentTokenBufferMemory,
+    )
+
+    # "This model's maximum context length is 4097 tokens
+    memory = AgentTokenBufferMemory(memory_key=memory_key, llm=get_llm(), max_token_limit=4096)
+
+    from langchain.agents.openai_functions_agent.base import OpenAIFunctionsAgent
+    from langchain.prompts import MessagesPlaceholder
+    from langchain.schema.messages import SystemMessage
+
+    system_message = SystemMessage(
+        content=(
+            "Do your best to answer the questions. "
+            "Feel free to use any tools available to look up "
+            "relevant information when needed, ALWAYS return the \"Source:\" part in your answer if there is a \"Source:\" in the relevant information."
+            "Use numbered list for steps, and don't miss any step."
+            "If you don't know the answer, just say that you don't know. Don't try to make up an answer. "
+        )
+    )
+    prompt = OpenAIFunctionsAgent.create_prompt(
+        system_message=system_message,
+        extra_prompt_messages=[MessagesPlaceholder(variable_name=memory_key)],
+    )
+
+    agent = OpenAIFunctionsAgent(llm=get_llm(), tools=tools, prompt=prompt)
+    from langchain.agents import AgentExecutor
+    agent_executor = AgentExecutor(
+        agent=agent,
+        tools=tools,
+        memory=memory,
+        verbose=True,
+        return_intermediate_steps=True,
+    )
+
+    # agent_executor = create_conversational_retrieval_agent(get_llm(), tools, verbose=True)
+
     question = ''
     while 'exit()' not in question:
         if question:
-            answer = rag_chain.invoke({"question": question, "chat_history": chat_history})
-            print("answer:", answer)
+            result = agent_executor({"input": question})
+            # print(result)
             # chat_history.extend([HumanMessage(content=question), answer])
-        question = input("Input your question ('exit()' to exit):")
+        question = input("Your input:")
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    main()
